974c476fca43d789669f063e85af8cebe075a24b
Thanh Nguyen <thanhnhotn2001@gmail.com>
2023-04-29 20:22:56 +0900

    assignment

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -8,18 +8,10 @@
 
 static void syscall_handler (struct intr_frame *);
 void halt(void);
-void exit(int stat);
-pid_t exec(const char *thread_name);
-int wait(pid_t pid);
-bool create(const char *file_name, unsigned starting_size);
-bool remove(const char *file_name);
-int open(const char *file_name);
-off_t fileSize(int fd);
-int read(int fd, const void *buffer, unsigned size);
-int write(int fd, const void *buffer, unsigned size);
-void seek(int fd, unsigned position);
-unsigned tell (int fd);
-void close(int fd);
+void exit(int status);
+pid_t exec (const char *cmd_line);
+int wait (pid_t pid);
+
 
 
 /* 
@@ -36,171 +28,43 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  // char *fn_copy;
-  // int size;
+  
+  uint32_t *p = f -> esp;
 
-  switch (f -> R.rax)
+  switch (*p)
   {
   case SYS_HALT:
-    printf("HALT!");
     halt();
     break;
   case SYS_EXIT:
-    printf("EXIT!");
-    int stat = *((int*) f -> esp + 1);
-    exit(stat);
+    int status = (int)(*(uint32_t*)(f -> esp + 4));
+    exit(status);
+    f -> eax = (int)(*(uint32_t*)(f -> esp + 4));
     break;
   case SYS_EXEC:
-    printf("EXEC!");
-    char *thread_name;
-    int r_code = exec((const char *)thread_name);
-    f -> eax = (uint32_t) r_code;
+    char *file_name =(char*)*(uint32_t*)(f -> esp + 4);
+    f -> eax = exec(file_name);
     break;
   case SYS_WAIT:
-    printf("WAIT!");
-    char *thread_name;
-    int r_code = wait((const char *)thread_name);
-    f -> eax = (uint32_t) r_code;
+    pid_t childPid = (pid_t)*(uint32_t*)(f -> esp + 4);
+    f -> eax = wait(childPid);
     break;
-  case SYS_CREATE:
-    printf("CREATE!");
-    const char *file_name = ((const char*)f -> esp + 1);
-    unsigned starting_size = *((unsigned *) f -> esp + 2);
-    f -> eax = create(file_name, starting_size);
-    break;
-  case SYS_REMOVE:
-    printf("REMOVE!");
-    char *file_name = f -> eax + 1;
-    f -> eax = remove(file_name);
-    break;
-  case SYS_OPEN:
-    printf("OPEN!");
-    char *file_name = f -> esp + 1;
-    f -> eax = open(file_name);
-    break;
-  case SYS_FILESIZE:
-    printf("FILESIZE!");
-    int fd_size_file = *((int*)f -> esp + 1);
-    f -> eax = fileSize(fd_size_file);
-    break;
-  case SYS_READ:
-    printf("READ!");
-    int *file_name = f -> esp + 1;
-    char *buff = f -> esp + 2;
-    unsigned *size = f -> esp + 3;
-    f -> eax = read((int)file_name, (void*)buff, (unsigned)size);
-    break;
-  case SYS_WRITE:
-    printf("WRITE!");
-    // f -> R.rax = write(f -> R.rdi, f -> R.rsi, f -> R.rdx);
-    break;
-  case SYS_SEEK:
-    printf("SEEK!");
-    // seek(f -> R.rdi, f -> R.rsi);
-    break;
-  case SYS_TELL:
-    printf("TELL!");
-    // f -> R.rax = tell(f -> R.rdi);
-    break;
-  case SYS_CLOSE:
-    printf("CLOSE!");
-    // close(f -> R.rdi);
-    break;
-  default:
-    exit(-1);
-    break;
-  }
-
-  printf ("system call!\n");
-  //thread_exit ();
 }
 
-void
-halt(void){
+void halt(void){
   shutdown_power_off();
 }
 
-void 
-exit (int status){
+void exit(int status){
   struct thread *curr_thread = thread_current();
+  printf("Name of process: %d", status);
   thread_exit();
 }
 
-pid_t 
-exec(const char *thread_name){
-  pid_t id = process_execute(thread_name);
-  return id;
+pid_t exec(const char *cmd_line){
+  return process_execute(cmd_line);
 }
 
-int 
-wait(pid_t pid){
-  int id = process_wait(pid);
-  return id;
+int wait(pid_t pid){
+  return process_wait(pid);
 }
-
-bool 
-create(const char *file_name, unsigned starting_size){
-  bool created = false;
-
-  if(file_name == NULL){
-    return false;
-  }else{
-    created = filesys_create(file_name, starting_size);
-    return created;
-  }
-  return created;
-}
-
-bool 
-remove(const char *file_name){
-  bool removed = false;
-
-  if(file_name == NULL){
-    return false;
-  }else{
-    removed = filesys_remove(file_name);
-    return removed;
-  }
-}
-
-int 
-open(const char *file_name){
-  struct file *open_file = filesys_open(file_name);
-  
-  if(open_file == NULL){
-    return -1;
-  }else{
-    int fd = set_file(open_file);
-    return fd;
-  }
-  return -1;
-}
-
-// off_t 
-// fileSize(int fd){
-//   struct process_file *curr_file = get_file(fd);
-//   off_t size_file = 0; 
-
-//   if(curr_file != NULL){
-//     size_file = file_length(curr_file -> file);
-//   }
-//   return size_file;
-// }
-
-int 
-read(int fd, const void *buffer, unsigned size){
-  int size_byte = 0;
-  check_address(buffer);
-  struct thread *curr_thread = thread_current();
-  struct file *curr_file = find_file_by_fd(fd);
-  
-  if(curr_file == NULL){
-    return -1;
-  }
-
-  }
-
-// void check_address(const uint64_t *pointer){
-//   struct thread *curr_thread = thread_current();
-//   if(pointer == NULL || )
-// }
