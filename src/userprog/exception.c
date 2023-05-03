ed519938950f378c9974a2df0e8fe4412dd07a63
Thanh Nguyen <thanhnhotn2001@gmail.com>
2023-05-03 20:00:26 +0900

    project2

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -152,12 +152,7 @@ page_fault (struct intr_frame *f)
    if(not_present){
       exit(-1);
    }
-   if(!user && !is_kernel_vaddr(fault_addr)){
-      exit(-1);
-   }
-   if(user && !is_user_vaddr(fault_addr)){
-      exit(-1);
-   }
+   
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
